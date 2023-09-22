abf6ef1ba810993c48cc2b6a8bcaec7287ad212b
BM-Daniel <danbabereyir@gmail.com>
2023-09-22 13:23:48 +0000

    Hmmm

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -10,7 +10,7 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  // console.log(`Serve listening at http://${hostname}:${port}`);
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-module.export = app;
+module.exports = app;
