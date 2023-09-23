228da5b84d0f3d4b3e7ca6a1fbdfc63bddd383e5
BM-Daniel <danbabereyir@gmail.com>
2023-09-23 10:46:01 +0000

    Hmmm

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,14 +5,8 @@ class StudentsController {
     readDatabase(DATABASE)
       .then((fields) => {
         const students = [];
-        // let count = 0;
         let msg;
 
-        // for (const key of Object.keys(fields)) {
-        //   count += fields[key].length;
-        // }
-
-        // students.push(`Number of students: ${count}`);
         students.push('This is the list of our students');
 
         for (const key of Object.keys(fields)) {
