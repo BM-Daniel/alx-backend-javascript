57d9d979c754c563076022e5f9bdb2f40c37549a
BM-Daniel <danbabereyir@gmail.com>
2023-10-27 10:54:20 +0000

    task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
+const sinon = require('sinon');
+
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('', () => {
+  let spyBoy;
+
+  beforeEach(() => {
+    sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('checking if numbers round', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spyBoy.calledOnce).to.be.true;
+    expect(spyBoy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it("checking if numbers round", () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spyBoy.calledOnce).to.be.true;
+    expect(spyBoy.calledWith("The total is: 20")).to.be.true;
+  });
+});
